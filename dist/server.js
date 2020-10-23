a9c0809fca181e4b137f6230a0b3ef121f5e0b1a
kandoka <kandoka@126.com>
2020-10-23 14:08:59 +0800

    oo

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const serveStatic = require('serve-static')
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8080
 
 app.use(serveStatic(__dirname))
 app.listen(port)
